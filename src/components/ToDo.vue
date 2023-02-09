8f2e12473ca4ea7374056af948cc359bf4215c95
Adriana de Lazzari <adrianaclazzari@gmail.com>
2023-02-09 17:03:55 -0500

    Adding todo list items

--- src/components/ToDo.vue
+++ src/components/ToDo.vue
@@ -1,17 +1,32 @@
 <template>
-    <div>
-      <div>Add a todo:</div>
-      <input type="text" v-model="value"/>
-      <p>{{ value }}</p>
-    </div>
-  </template>
-  
-  <script>
-  export default{
-    data(){
-      return{
-        value: undefined
-      }
-    }
-  }
-  </script>
+  <div>
+    <div>Add a todo:</div>
+    <input type="text" v-model="value" />
+    <button @click="addToDo">Add</button>
+  </div>
+  <div>
+    <div>My todo list:</div>
+    <ul>
+      <li v-for="(item, itemIndex) in toDoList" :key="itemIndex">
+        {{ item }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      toDoList: [],
+      value: undefined,
+    };
+  },
+  methods: {
+    addToDo() {
+      this.toDoList.push(this.value);
+      this.value = undefined;
+    },
+  },
+};
+</script>
