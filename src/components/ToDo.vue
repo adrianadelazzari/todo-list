a8546e2222ad65c4d658a49582fe8835b0a6de86
Adriana de Lazzari <adrianaclazzari@gmail.com>
2023-02-09 16:55:45 -0500

    Adding to do component

---
+++ src/components/ToDo.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+      <div>Add a todo:</div>
+      <input type="text" v-model="value"/>
+      <p>{{ value }}</p>
+    </div>
+  </template>
+  
+  <script>
+  export default{
+    data(){
+      return{
+        value: undefined
+      }
+    }
+  }
+  </script>
